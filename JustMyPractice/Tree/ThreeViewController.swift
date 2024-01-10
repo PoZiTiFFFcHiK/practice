632274442f85bafb7966dbcf51dc5f8c51f236b1
Vladimir Petrov <VPetrov2@alfabank.ru>
2024-01-10 18:26:03 +0100

    Deleted unused code

--- JustMyPractice/Tree/ThreeViewController.swift
+++ JustMyPractice/Tree/ThreeViewController.swift
@@ -15,35 +15,9 @@ final class TreeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureNavigationBar()
     }
     
     override func loadView() {
         view = contentView
-    }
-}
-
-private extension TreeViewController {
-    func configureNavigationBar() {
-        let plusItem = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .plain, target: self, action: #selector(didTapPlusButton))
-        navigationItem.rightBarButtonItems = [plusItem]
-    }
-    
-    @objc
-    func didTapPlusButton() {
-        let alertViewController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-        
-        let addPartnerAction = UIAlertAction(title: "Add a partner", style: .default) { [weak self] _ in
-            // self?.addPartner()
-        }
-        let addChildAction = UIAlertAction(title: "Add a child", style: .default) { [weak self] _ in
-            // self?.addChild()
-        }
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { [weak self] _ in
-            self?.dismiss(animated: true)
-        }
-        
-        [addPartnerAction, addChildAction, cancelAction].forEach { alertViewController.addAction($0) }
-        present(alertViewController, animated: true)
     }
 }
